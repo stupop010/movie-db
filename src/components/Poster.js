b71f41600991306c6ae49b845b68cc4d0b81122c
Stuart <stupop010@hotmail.co.uk>
2019-08-05 20:51:08 +0100

    added better responsive

--- src/components/Poster.js
+++ src/components/Poster.js
@@ -1,6 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const Poster = ({ poster }) => {
+  const [show, setShow] = useState(true);
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+  });
+
+  const handleResize = () => {
+    if (window.innerWidth <= 600) {
+      setShow(false);
+    } else {
+      setShow(true);
+    }
+  };
+
+  if (!show) return null;
+
   return (
     <div
       className="movie-poster"
